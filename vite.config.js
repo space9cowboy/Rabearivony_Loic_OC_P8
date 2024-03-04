50e610562edffea4b88e147ae13f500d5c908e69
Rabearivony Loïc <122444363+space9cowboy@users.noreply.github.com>
2024-03-04 15:18:07 +0100

    final portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // server: {
-  //   port: 8686,
-  // },
+  build: {
+    outDir: "portfolio",
+    rollupOptions: {
+      input: ["src/main.jsx", "./index.html"],
+    },
+  },
 });
